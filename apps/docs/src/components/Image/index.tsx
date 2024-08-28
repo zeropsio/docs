a0dd67e45603601e017040b14b06076a0d3c928a
rTanwar <meow@nermalcat69.dev>
2024-08-29 02:31:21 +0530

    chore: dark mode image

---
+++ apps/docs/src/components/Image/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import { useColorMode } from '@docusaurus/theme-common';
+
+type ImageProps = {
+  lightImage: string;
+  darkImage: string;
+  alt: string;
+  [key: string]: any;
+};
+
+const Image: React.FC<ImageProps> = ({ lightImage, darkImage, alt, ...props }) => {
+  const { colorMode } = useColorMode();
+
+  const imageUrl = colorMode === 'dark' ? darkImage : lightImage;
+
+  return <img src={useBaseUrl(imageUrl)} alt={alt} {...props} />;
+};
+
+export default Image;
