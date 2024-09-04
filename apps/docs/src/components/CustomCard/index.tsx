2e3635f55edf3af9ed865a148b34c73fb7a0157b
Nermal <meow@nermalcat69.dev>
2024-09-04 17:27:26 +0530

    chore: fix pr template + add bun.lockb + some linting
    * chore: add bun for installation
    
    * Update PULL_REQUEST_TEMPLATE.md
    
    * Update build.yml
    
    * chore: linting

--- apps/docs/src/components/CustomCard/index.tsx
+++ apps/docs/src/components/CustomCard/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react"
 
 interface CustomCardProps {
-  emoji: string;
-  title: string;
-  children: React.ReactNode;
+  emoji: string
+  title: string
+  children: React.ReactNode
 }
 
 const CustomCard: React.FC<CustomCardProps> = ({ title, children, emoji }) => {
@@ -11,15 +11,11 @@ const CustomCard: React.FC<CustomCardProps> = ({ title, children, emoji }) => {
     <div className="bg-medusa-tag-neutral-bg border-medusa-tag-neutral-border flex flex-row py-1 px-1 my-2  border border-solid rounded shadow-none">
       <span className="mr-1">{emoji}</span>
       <div>
-      <span className="font-semibold text-md">
-        {title}
-      </span>
-      <div className="pb-0 pt-px -mb-1">
-        {children}
-      </div>   
+        <span className="font-semibold text-md">{title}</span>
+        <div className="pb-0 pt-px -mb-1">{children}</div>
       </div>
     </div>
-  );
+  )
 }
 
-export default CustomCard;
+export default CustomCard
--- apps/docs/src/components/UnorderedList/index.tsx
+++ apps/docs/src/components/UnorderedList/index.tsx
@@ -3,16 +3,10 @@ import type { Props } from "@theme/DocItem/Layout"
 
 export default function UnorderedList({ data }: Props): JSX.Element {
   return (
-        <ul>
-        {
-          data.map((token, i) => {
-            return(
-              <li>
-                {token}
-              </li>
-            )
-          })
-        }
+    <ul>
+      {data.map((token, i) => {
+        return <li>{token}</li>
+      })}
     </ul>
   )
 }
