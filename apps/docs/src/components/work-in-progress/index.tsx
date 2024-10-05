aadca4cf463898b52f7eb1d7ce2da5cbcce23ca3
Arjun Aditya <meow@nermalcat69.dev>
2024-10-06 02:24:08 +0530

    chore: fix work in progress btn

--- apps/docs/src/components/work-in-progress/index.tsx
+++ apps/docs/src/components/work-in-progress/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Wip() {
   return (
-    <div className="fixed px-2 py-0.5 bottom-2 bg-medusa-tag-neutral-bg border-medusa-tag-neutral-border border border-solid rounded shadow-none duration-150 cursor-progress hover:scale-105 right-2">
+    <div className="fixed z-40 px-2 py-0.5 bottom-2 bg-gray-100 border-medusa-tag-neutral-border border border-solid rounded shadow-none duration-150 cursor-progress hover:scale-105 right-2">
       🚧 Work in Progress
     </div>
   );
