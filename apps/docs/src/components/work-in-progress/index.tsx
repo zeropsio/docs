259bbfd43afa073e66922932fd6aab0fcab987e0
Arjun Aditya <meow@nermalcat69.dev>
2024-12-04 05:19:35 +0530

    chore: add link to wip component + add github link to contacts page

--- apps/docs/src/components/work-in-progress/index.tsx
+++ apps/docs/src/components/work-in-progress/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Tooltip } from 'docs-ui';
 
 function Wip() {
   return (
-    <div className="fixed z-40 px-2 py-0.5 bottom-2 bg-gray-100 dark:bg-neutral-800 border-medusa-tag-neutral-border border border-solid rounded shadow-none duration-150 cursor-progress hover:scale-105 right-2">
-      🚧 Work in Progress
+    <div className="fixed z-40 bottom-2 right-4">
+      <Tooltip
+        text="😭 Confused? Click to contact us."
+        positionStrategy="absolute"
+        place="top"
+      >
+        <a href="https://docs.zerops.io/help/contacts">
+          <span
+            className="cursor-pointer px-1 text-lg text-neutral-900 dark:text-neutral-200 py-0.5 bg-gray-100 dark:bg-neutral-800 border-medusa-tag-neutral-border border border-solid rounded shadow-none duration-150 hover:scale-105 inline-block"
+          >
+            🚧 Work in Progress
+          </span>
+        </a>
+      </Tooltip>
     </div>
   );
 }
