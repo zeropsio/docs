d6d3880999515b756e0e71f2bfde0a5f22986f46
vankovap <petravankova.pv@gmail.com>
2024-12-14 20:45:26 +0100

    base list (#115)
    * base list
    
    * link fixes

--- apps/docs/src/components/UnorderedCodeList/index.tsx
+++ apps/docs/src/components/UnorderedCodeList/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { InlineCode } from 'docs-ui';
 
 interface Props {
-  data: string[];
+  data: (string | string[])[];
 }
 
 export default function UnorderedCodeList({ data }: Props): JSX.Element {
   return (
     <ul>
-      {data.map((token: string, index: number) => {
-        return (
-          <li key={index}>
-            <code>{token}</code>
-          </li>
-        );
-      })}
+      {data.map((token, index) => (
+        <li key={index}>
+          {Array.isArray(token) ? (
+            // If token is an array, render each item in a separate <code> block
+            token.map((item, subIndex) => (
+              <React.Fragment key={subIndex}>
+                <InlineCode>{item}</InlineCode>
+                {subIndex < token.length - 1 && ', '}
+              </React.Fragment>
+            ))
+          ) : (
+            // If token is a string, render it in a single <code> block
+            <InlineCode>{token}</InlineCode>
+          )}
+        </li>
+      ))}
     </ul>
   );
 }
