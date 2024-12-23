3d3c230efdbd614cfb1d8c24bfd5e33f019dcea6
Jan Saidl <saidl@vshosting.cz>
2024-12-23 23:10:51 +0100

    Elasticsearch backup and generic backup page

--- apps/docs/sidebars.js
+++ apps/docs/sidebars.js
@@ -97,6 +97,15 @@ module.exports = {
       },
       className: 'homepage-sidebar-item',
     },
+    {
+      type: 'doc',
+      id: 'features/backup',
+      label: 'Backup',
+      customProps: {
+        sidebar_icon: 'archive-box',
+      },
+      className: 'homepage-sidebar-item',
+    },
     // {
     //   type: 'doc',
     //   id: 'features/remote-dev',
