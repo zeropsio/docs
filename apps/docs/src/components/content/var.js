67b15aecb712648cadbd14fb5c2e7493c93452a7
vankovap <petravankova.pv@gmail.com>
2025-06-11 10:35:58 +0200

    reusable 3 (#266)

--- apps/docs/src/components/content/var.js
+++ apps/docs/src/components/content/var.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CodeBlock from '@theme/CodeBlock';
 
 const variables = {};
 
@@ -15,6 +16,12 @@ export const VarLink = ({ name, path, children }) => {
   const value = variables[name] || '';
   const href = path.replace('{{VAR}}', value);
   return <a href={href}>{children}</a>;
+};
+
+export const VarCodeBlock = ({ codeVar, languageVar }) => {
+  const code = variables[codeVar] || '';
+  const language = variables[languageVar] || 'javascript';
+  return <CodeBlock language={language}>{code}</CodeBlock>;
 };
 
 export default Var;
