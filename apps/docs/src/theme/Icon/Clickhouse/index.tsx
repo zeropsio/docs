b98b65977fa5c59e42bfca42af007344cf2f0c00
Petra Vankova <petravankova.pv@gmail.com>
2025-08-06 09:44:08 +0200

    icons fix

--- apps/docs/src/theme/Icon/Clickhouse/index.tsx
+++ apps/docs/src/theme/Icon/Clickhouse/index.tsx
@@ -4,17 +4,15 @@ import clsx from 'clsx';
 import React from 'react';
 
 const IconClickhouse = (props: IconProps) => {
-  const { colorMode } = useColorMode();
-  const isDark = colorMode === 'dark';
   return (
     <svg
       width={props.width || 20}
       height={props.height || 20}
       viewBox="0 0 150 150"
-      fill={isDark ? '#ffffff' : '#161616'}
+      fill="#000000"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
-      className={clsx('text-ui-fg-subtle', props.className)}
+      className={clsx('mono-icon', props.className)}
     >
         <path d="M27,25.7c0-.6.5-1.2,1.2-1.2h8.8c.6,0,1.2.5,1.2,1.2v97.7c0,.6-.5,1.2-1.2,1.2h-8.8c-.6,0-1.2-.5-1.2-1.2V25.7Z"/>
         <path d="M49.2,25.7c0-.6.5-1.2,1.2-1.2h8.8c.6,0,1.2.5,1.2,1.2v97.7c0,.6-.5,1.2-1.2,1.2h-8.8c-.6,0-1.2-.5-1.2-1.2V25.7Z"/>
